b37ab4a145b2a0b859024076dbd03374e0189543
webioann <webioann@gmail.com>
2025-08-15 22:27:51 +0300

    issues was fixed

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    mode: "development", // or 'production'
+    devtool: 'inline-source-map', // for development, use 'source-map' for production
     entry: {
         main: path.resolve(__dirname, './src/main.ts'),
     },
@@ -12,17 +14,18 @@ module.exports = {
     },
     devServer: {
         static: {
-          directory: path.join(__dirname, 'dist'), // Or your desired content base
+          directory: path.resolve(__dirname, 'dist'), // Or your desired content base
         },
         port: 8080, // Or your preferred port
         open: true,
         historyApiFallback: true,
-        hot: true, // Enable Hot Module Replacement
+        hot: true, 
+        compress: true, // Enable gzip compression
     },
         plugins: [
         new HtmlWebpackPlugin({
             title: 'webpack Boilerplate',
-            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            template: path.resolve(__dirname, './src/template.html'), // pattern to your HTML template
             filename: 'index.html', // output file
             inject: 'body', // script tags will be injected into the body
         }),    
@@ -37,13 +40,14 @@ module.exports = {
             {
                 test: /\.(s*)css$/i,
                 use: [
-                    MiniCssExtractPlugin.loader, "css-loader", "sass-loader"
+                    "style-loader", "css-loader", "sass-loader"
+                    // MiniCssExtractPlugin.loader, "css-loader", "sass-loader"
                 ],
                 // use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.ts?$/,
-                use: ['ts-loader', 'babel-loader'],
+                use: ['ts-loader'],
                 exclude: /node_modules/,
             },
              // images rules 
