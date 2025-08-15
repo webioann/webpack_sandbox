ad651660d738abf0184c8ad41ce5a77622d31240
webioann <webioann@gmail.com>
2025-08-15 22:47:25 +0300

    sucsessfuly setup typescript and sass

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 // import './template.html';
 import './styles.scss';
 
-console.log('Hello there!');
-// document.getElementById('root').innerHTML = '<h1>Hello Webpack with CSS!</h1>';
+type nameList = 'John' | 'Ioann' | 'Lucifer';
+
+type Person = {
+    name: nameList;
+    age: number;
+};
+let person: Person = {
+    name: 'John',
+    age: 30,
+};
+
+console.log(`Hello, my name is ${person.name} and I am ${person.age} years old.`);
+
+const newDiv = document.createElement('div');
+newDiv.textContent = `Hello, my name is ${person.name} and I am ${person.age} years old.`;
+newDiv.className = 'new-div';
+document.body.appendChild(newDiv);
